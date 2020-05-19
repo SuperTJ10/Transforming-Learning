df89e425022290050118536e15d90c8129e1f4d9
Tim Marshall <supertj10@gmail.com>
2020-05-19 10:57:33 +0100

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,169 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ldc3E5eOTc":
+        Script1();
+        break;
+      case "6ScpJadVz4J":
+        Script2();
+        break;
+      case "6mnxgdvVsJn":
+        Script3();
+        break;
+      case "66tF8TpFuqd":
+        Script4();
+        break;
+      case "5uBaKg1pUoJ":
+        Script5();
+        break;
+      case "6q57d6ko6Ay":
+        Script6();
+        break;
+      case "5kZso1Gz2Bf":
+        Script7();
+        break;
+      case "6ZU00rFmX7S":
+        Script8();
+        break;
+      case "651JTEZDxbU":
+        Script9();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var email = 'PtCTraining@Nats.co.uk'
+
+var courseTitle = player.GetVar('feedback_courseTitle');
+var slideNum = player.GetVar('feedback_slideNum');
+var slideTitle = player.GetVar('feedback_slideTitle');
+
+var subject = 'BUG REPORT: ' + courseTitle;
+var emailBody = 'I have spotted a bug within the eLearning content above.' + '\n\nThe bug can be found on slide ' + slideNum + ' (' + slideTitle + ')' + '\n\nPlease include additional information below:\n\n';
+var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(emailBody);
+win = window.open(mailto_link, 'emailWin');
+}
+
